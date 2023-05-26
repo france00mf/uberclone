ed8f4f95fd28e8b44a9d6cfc1c4b8ca7ab5b034d
France <franciscodomingosjose06798@gmail.com>
2023-05-26 10:50:46 +0100

    add auths login files.

---
+++ lib/presenter/screem/auth_screem/login_screem.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LoginScreem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView();
+  }
+}
