f27f209407a7228eb87f06cd1813314e3ff2a616
France <franciscodomingosjose06798@gmail.com>
2023-05-29 12:57:25 +0100

    Add Register Screem to route

--- lib/app/widgets/routes_widget.dart
+++ lib/app/widgets/routes_widget.dart
@@ -1,9 +1,11 @@
 import 'dart:js';
 
 import 'package:flutter/material.dart';
+import 'package:uberclone/presenter/screem/auth_screem/register_screem.dart';
 
 import '../../presenter/screem/screem.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   LoginScreem.page: (context) => LoginScreem(),
+  RegisterScreem.page: (context) => RegisterScreem(),
 };
