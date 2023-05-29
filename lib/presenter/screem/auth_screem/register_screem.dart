e8cc9e466c96f14379746c1339b854319382d841
France <franciscodomingosjose06798@gmail.com>
2023-05-29 11:46:32 +0100

    create register page

---
+++ lib/presenter/screem/auth_screem/register_screem.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreem extends StatelessWidget {
+  static String page = "page";
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Center(
+            child: Text("Register Page, UBER CLONE ANGOLA"),
+          )
+        ],
+      ),
+    );
+  }
+}
