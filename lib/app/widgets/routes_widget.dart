0c530db18d69da6b97b42dc6a763b65ae09468be
France <franciscodomingosjose06798@gmail.com>
2023-05-30 11:25:20 +0100

    assign home on route

--- lib/app/widgets/routes_widget.dart
+++ lib/app/widgets/routes_widget.dart
@@ -5,4 +5,6 @@ import '../../presenter/screem/screem.dart';
 Map<String, Widget Function(BuildContext)> routes = {
   LoginScreem.idScreem: (context) => LoginScreem(),
   RegisterScreem.idScreem: (context) => RegisterScreem(),
+  HomeScreemView.idScreem: (context) =>
+      HomeScreemView(title: "Uber Clone Home page")
 };
