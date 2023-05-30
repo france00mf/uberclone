482de830036fdbc8f33e24a6b1aaa6dbceb44c64
France <franciscodomingosjose06798@gmail.com>
2023-05-30 11:25:28 +0100

    add new files

--- lib/presenter/screem/home_screem/home_screem_view.dart
+++ lib/presenter/screem/home_screem/home_screem_view.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 
 class HomeScreemView extends StatefulWidget {
   const HomeScreemView({Key? key, required this.title}) : super(key: key);
 
   final String title;
+  static String idScreem = "home_screem_view";
 
   @override
   State<HomeScreemView> createState() => _HomeScreemViewState();
 }
 
 class _HomeScreemViewState extends State<HomeScreemView> {
+  var msgHome;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    msgHome = widget.title;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,8 +31,8 @@ class _HomeScreemViewState extends State<HomeScreemView> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Uber Clone Home page',
+            Text(
+              msgHome.toString(),
             ),
           ],
         ),
