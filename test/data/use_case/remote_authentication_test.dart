ea84a5f928e8d5eea9b8c54f69eb04e2acf179ce
France <franciscodomingosjose06798@gmail.com>
2023-06-15 11:48:46 +0100

    fix: put remoteauths test on own path

--- test/data/use_case/remote_authentication_test.dart
+++ test/data/use_case/remote_authentication_test.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 
 class RemoteAuthentication {
-  Future<void> auth({String? email, String? password}) {
-    return this.auth();
+  Future<void> auth() {
+  
   }
 }
 
