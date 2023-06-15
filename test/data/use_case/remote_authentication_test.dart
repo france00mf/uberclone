3f35bd2c681e265ea9c931bb7cfe016aeaa32f78
France <franciscodomingosjose06798@gmail.com>
2023-06-15 12:18:41 +0100

    fix: make test pass

--- test/data/use_case/remote_authentication_test.dart
+++ test/data/use_case/remote_authentication_test.dart
@@ -1,15 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
 
 class RemoteAuthentication {
-  auth() {}
-}
-class HttpClient{
-  request(){}
+  void auth() {}
 }
 
+class HttpClient {
+  void request() {}
+}
+
+// class HttpClientSpy extends Mock implements HttpClient {}
+
 void main() {
-  test('Should be login with corrects values on httpClient and url'), () async {
+  test(
+      'Should be login with corrects values on RemoteAuthentication and httpClient',
+      () async {
     RemoteAuthentication sut = RemoteAuthentication();
-    await sut.auth();
+    HttpClient httpClient = HttpClient();
+    sut.auth();
+    // verify(httpClient.request()).called(1);
   });
 }
