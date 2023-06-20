fada6b1886d9882e88555d72e0ed54e64d6c94ad
France <franciscodomingosjose06798@gmail.com>
2023-06-20 11:57:13 +0100

    remote_authentication to own file

---
+++ lib/data/usecases/remote_authentication.dart
@@ -0,0 +1,10 @@
+class RemoteAuthentication {
+  final HttpClient? httpClient;
+  final String? url;
+
+  RemoteAuthentication({@required this.httpClient, @required this.url});
+  Future<dynamic> auth(Map? body) async {
+    print("RemoteAuth Url's -> " + url.toString());
+    await httpClient!.request(url: url, mathod: 'post', body: body);
+  }
+}
