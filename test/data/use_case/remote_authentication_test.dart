c4d712cbf7f789f66006dbba4155f588dc8fb1a3
France <franciscodomingosjose06798@gmail.com>
2023-06-20 11:57:00 +0100

    move remote_authentication to own file and path

--- test/data/use_case/remote_authentication_test.dart
+++ test/data/use_case/remote_authentication_test.dart
@@ -2,17 +2,6 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class RemoteAuthentication {
-  final HttpClient? httpClient;
-  final String? url;
-
-  RemoteAuthentication({@required this.httpClient, @required this.url});
-  Future<dynamic> auth(Map? body) async {
-    print("RemoteAuth Url's -> " + url.toString());
-    await httpClient!.request(url: url, mathod: 'post', body: body);
-  }
-}
-
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
